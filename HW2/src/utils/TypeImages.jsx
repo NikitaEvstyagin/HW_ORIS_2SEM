db0c3f65a301f2f63d8422ad3bf78c0f53ac9ec6
NikitaEvstyagin <48449329+NikitaEvstyagin@users.noreply.github.com>
2024-05-09 17:15:11 +0300

    Add files via upload

---
+++ HW2/src/utils/TypeImages.jsx
@@ -0,0 +1,39 @@
+import grass from "../assets/types/Grass.png";
+import fire from "../assets/types/Fire.png";
+import water from "../assets/types/Water.png";
+import electric from "../assets/types/Electric.png";
+import psychic from "../assets/types/Psychic.png";
+import fighting from "../assets/types/Fight.png";
+import flying from "../assets/types/Flying.png";
+import poison from "../assets/types/Poison.png";
+import ground from "../assets/types/Ground.png";
+import rock from "../assets/types/Rock.png";
+import bug from "../assets/types/Bug.png";
+import ice from "../assets/types/Ice.png";
+import ghost from "../assets/types/Ghost.png";
+import dark from "../assets/types/Dark.png";
+import steel from "../assets/types/Steel.png";
+import dragon from "../assets/types/Dragon.png";
+import normal from "../assets/types/Normal.png";
+import fairy from "../assets/types/Fairy.png";
+
+export default {
+    "grass": grass,
+    "fire": fire,
+    "water": water,
+    "electric": electric,
+    "psychic": psychic,
+    "fighting": fighting,
+    "flying": flying,
+    "poison": poison,
+    "ground": ground,
+    "rock": rock,
+    "bug": bug,
+    "ice": ice,
+    "ghost": ghost,
+    "dark": dark,
+    "steel": steel,
+    "dragon": dragon,
+    "normal": normal,
+    "fairy": fairy
+}
